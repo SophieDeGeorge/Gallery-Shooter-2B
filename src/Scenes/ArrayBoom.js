fac94d1feb4a4eb7496f5780859394b281253a09
SophieDeGeorge <sophiedegeorge142@gmail.com>
2025-06-11 18:33:18 -0700

    Hippo Enemy

--- src/Scenes/ArrayBoom.js
+++ src/Scenes/ArrayBoom.js
@@ -13,6 +13,9 @@ class ArrayBoom extends Phaser.Scene {
         // This array will hold bindings (pointers) to bullet sprites
         this.my.sprite.bullet = [];   
         this.maxBullets = 10;           // Don't create more than this many bullets
+
+        this.my.sprite.enemyBullet = [];
+        this.maxenemyBullets = 10;
         
         this.myScore = 0;       // record a score as a class variable
         // More typically want to use a global variable for score, since
@@ -43,13 +46,6 @@ class ArrayBoom extends Phaser.Scene {
         this.load.audio("dadada", "jingles_NES13.ogg");
     }
 
-    addPoint(point) {
-        let i = 0;
-            for (this.point in this.points) {
-                this.curve.points[i] = point;
-                i++;
-            }
-    }
 
 
     create() {
@@ -59,30 +55,37 @@ class ArrayBoom extends Phaser.Scene {
                                                                     //INIT
 
             // Path points
-        this.points = [
+        this.hippo1Points = [
             20, 20,
-            80, 400,
-            300, 750
+            750, 100,
+            20, 200,
+            750, 300,
+            20, 400,
+            700, 700
         ];
 
             // Create spline curve
-        this.curve = new Phaser.Curves.Spline(this.points);
+        this.curve = new Phaser.Curves.Spline(this.hippo1Points);
 
             // Phaser graphics
         this.graphics = this.add.graphics();
 
 
-        let followConfig = {
-            from: 0,
-            to: 1,
-            delay: 0,
-            duration: 2000,
-            ease: 'Sine.easeInOut',
-            repeat: -1,
-            yoyo: true,
-            rotateToPath: true,
-            rotationOffset: -90
-        }
+        this.lives = 3;
+        this.playerHit = false;
+        this.iFrames = 2000;
+        this.invincibilityTimer = 0;
+
+        this.hippo1ShootTimer = 10000;
+        this.hippo1ShootTime = 10000;
+
+        this.hippo1dead = false;
+
+        
+        this.resetTime = 20000;
+        this.resetTimer = this.resetTime;
+        this.lost = false;
+        this.hippoWaves = 1;
 
                                                                     //INIT END
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -95,9 +98,10 @@ class ArrayBoom extends Phaser.Scene {
         my.sprite.elephant = this.add.sprite(game.config.width/2, game.config.height - 40, "elephant");
         my.sprite.elephant.setScale(0.25);
 
-        my.sprite.hippo = this.add.follower(this.curve, game.config.width/2, 80, "hippo");
-        my.sprite.hippo.setScale(0.25);
-        my.sprite.hippo.scorePoints = 25;
+        this.hippo1 = this.add.follower(this.curve, game.config.width/2, 80, "hippo");
+        this.hippo1.setScale(0.25);
+        this.hippo1.scorePoints = 25;
+        
 
 
                                                                     //SPRITES END
@@ -135,6 +139,7 @@ class ArrayBoom extends Phaser.Scene {
         this.left = this.input.keyboard.addKey("A");
         this.right = this.input.keyboard.addKey("D");
         this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.rKey = this.input.keyboard.addKey("R");
 
                                                                     //KEY OBJECTS END
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -142,6 +147,7 @@ class ArrayBoom extends Phaser.Scene {
         // Set movement speeds (in pixels/tick)
         this.playerSpeed = 15;
         this.bulletSpeed = 35;
+        this.enemyBulletSpeed = 15;
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -152,6 +158,7 @@ class ArrayBoom extends Phaser.Scene {
 
         // Put score on screen
         my.text.score = this.add.bitmapText(580, 0, "rocketSquare", "Score " + this.myScore);
+        my.text.lives = this.add.bitmapText(580, 30, "rocketSquare", "Lives " + this.lives);
 
         // Put title on screen
         this.add.text(10, 5, "Hippo Hug!", {
@@ -164,18 +171,32 @@ class ArrayBoom extends Phaser.Scene {
 
                                                                     //TEXT END
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        this.addPoint();
     }
 
 
 
 
-
-    
-
-    update() {
+    update(time, delta) {
         let my = this.my;
-
+        //console.log(this.hippo1ShootTimera);
+        this.hippo1ShootTimer -= delta * 3;
+        if (this.lost == true) {
+            this.resetTimer -= delta * 3;
+        }
+        if (this.resetTimer <= 0) {
+            this.scene.restart()
+        }
+        if (this.hippo1dead == true) {
+            this.hippo1.stopFollow();
+            this.hippo1.y = -100;
+            this.hippo1dead;
+        }
+        if (this.playerHit == true) {
+            this.invincibilityTimer -= delta * 3;
+                if (this.invincibilityTimer <= 0) {
+                    this.playerHit = false;
+                }
+        }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                                                                     //MOVEMENT
@@ -213,6 +234,17 @@ class ArrayBoom extends Phaser.Scene {
             }
         }
 
+//
+            //enemy shooting
+        if (this.hippo1ShootTimer <= 0) {
+            // Are we under our bullet quota?
+            if (my.sprite.enemyBullet.length < this.maxenemyBullets) {
+                my.sprite.enemyBullet.push(this.add.sprite(this.hippo1.x, this.hippo1.y-(this.hippo1.displayHeight/2), "heart"));
+                console.log(this.hippo1ShootTimer);
+                this.hippo1ShootTimer = this.hippo1ShootTime;
+            }
+        }
+//
                                                                     //SHOOTING END
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -221,57 +253,108 @@ class ArrayBoom extends Phaser.Scene {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                                                                     //COLLISIONS
 
-        // Remove all of the bullets which are offscreen
-        // filter() goes through all of the elements of the array, and
-        // only returns those which **pass** the provided test (conditional)
-        // In this case, the condition is, is the y value of the bullet
-        // greater than zero minus half the display height of the bullet? 
-        // (i.e., is the bullet fully offscreen to the top?)
-        // We store the array returned from filter() back into the bullet
-        // array, overwriting it. 
-        // This does have the impact of re-creating the bullet array on every 
-        // update() call. 
+            // get rid of bullets past boundaries
         my.sprite.bullet = my.sprite.bullet.filter((bullet) => bullet.y > -(bullet.displayHeight/2));
+        my.sprite.enemyBullet = my.sprite.enemyBullet.filter((enemyBullet) => enemyBullet.y > (enemyBullet.displayHeight));
 
         // Check for collision with the hippo
         for (let bullet of my.sprite.bullet) {
-            if (this.collides(my.sprite.hippo, bullet)) {
+            if (this.collides(this.hippo1, bullet)) {
                 // start animation
-                this.puff = this.add.sprite(my.sprite.hippo.x, my.sprite.hippo.y, "whitePuff03").setScale(0.25).play("puff");
+                this.puff = this.add.sprite(this.hippo1.x, this.hippo1.y, "whitePuff03").setScale(0.25).play("puff");
                 // clear out bullet -- put y offscreen, will get reaped next update
                 bullet.y = -100;
-                my.sprite.hippo.visible = false;
-                my.sprite.hippo.x = -100;
+                //this.hippo1.visible = false;
+                this.hippo1.y = -100;
                 // Update score
-                this.myScore += my.sprite.hippo.scorePoints;
+                this.myScore += this.hippo1.scorePoints;
                 this.updateScore();
                 // Play sound
                 this.sound.play("dadada", {
                     volume: 0.20
-                });
+                }); 
+                this.hippo1dead = true;
                 // Have new hippo appear after end of animation
+                /*
                 this.puff.on(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
-                    this.my.sprite.hippo.visible = true;
-                    this.my.sprite.hippo.x = Math.random()*config.width;
-                }, this);
+                    this.hippo1.visible = true;
+                    this.hippo1.x = Math.random()*config.width;
+                }, this); */
 
             }
         }
 
-                                                                    //COLLISIONS END
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                        console.log("Outside curvepoints");
-        if (this.curve.points[0]) {
-            my.sprite.hippo.x = this.curve.points[0].x;
-            my.sprite.hippo.y = this.curve.points[0].y;
-        } else {
-                        console.log("No curve points");
-            my.sprite.hippo.x = 0;
-            my.sprite.hippo.y = 0;
+        // collision check bullet-player
+        for (let enemyBullet of my.sprite.enemyBullet) {
+            if (this.collides(my.sprite.elephant, enemyBullet)) {
+                // start animation
+                this.puff = this.add.sprite(my.sprite.elephant.x, my.sprite.elephant.y, "whitePuff03").setScale(0.25).play("puff");
+                // clear out bullet -- put y offscreen, will get reaped next update
+                enemyBullet.y = -1000;
+                this.lives--;
+                this.updateLives();
+                // Play sound
+                this.sound.play("dadada", {
+                    volume: 0.20
+                }); 
+
+            }
         }
 
+        // check for player-hippo collision
         
-        my.sprite.hippo.startFollow(followConfig);
+            if (this.collides(this.hippo1, my.sprite.elephant) && (this.invincibilityTimer <= 0)) {
+                this.playerHit = true;
+                this.invincibilityTimer = this.iFrames;
+                this.puff = this.add.sprite(this.hippo1.x, this.hippo1.y, "whitePuff03").setScale(0.25).play("puff");
+                this.hippo1.stopFollow();
+                console.log("stopfollow");
+                this.hippo1.x = -100;
+                console.log("move hippo");
+                this.hippo1dead = true;
+                
+                    // Update Lives
+                
+                this.lives -= 1;
+                this.updateLives();
+                this.sound.play("dadada", {
+                    volume: 0.20
+                });
+            }
+        
+
+                                                                    //COLLISIONS END
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        if (this.hippoWaves > 0) {
+                        console.log("Outside curvepoints");
+            if (this.curve.points[0]) {
+                this.hippo1.x = this.curve.points[0].x;
+                this.hippo1.y = this.curve.points[0].y;
+            } else {
+                            console.log("No curve points");
+                this.hippo1.x = 0;
+                this.hippo1.y = 0;
+            }
+
+            this.followConfig = {
+                from: 0,
+                to: 1,
+                delay: 0,
+                duration: 15000,
+                ease: 'Linear',
+                repeat: -1,
+                yoyo: false,
+                rotateToPath: true,
+                rotationOffset: -90
+            }
+            console.log("config");
+            if (this.hippo1dead == false) {
+                this.hippoWaves--;
+                this.hippo1.startFollow(this.followConfig);
+            }
+        }
 
 
 
@@ -279,6 +362,9 @@ class ArrayBoom extends Phaser.Scene {
         // Make all of the bullets move
         for (let bullet of my.sprite.bullet) {
             bullet.y -= this.bulletSpeed;
+        }
+        for (let enemyBullet of my.sprite.enemyBullet) {
+            enemyBullet.y += this.enemyBulletSpeed;
         }
 
     }
@@ -294,6 +380,19 @@ class ArrayBoom extends Phaser.Scene {
         let my = this.my;
         my.text.score.setText("Score " + this.myScore);
     }
+    youLost() {
+        let my = this.my;
+        my.text.lose = this.add.bitmapText(300, 300, "rocketSquare", "YOU LOSE! ");
+        this.lost = true;
+    }
+    updateLives() {
+        let my = this.my;
+        my.text.lives.setText("Lives " + this.lives);
+        if (this.lives <= 0) {
+            this.youLost();
+        }
+    }
+
 
 }
          
---
+++ src/Sprites/EnemyBullet.js
@@ -0,0 +1,28 @@
+class EnemyBullet extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {        
+        super(scene, x, y, texture, frame);
+        this.visible = false;
+        this.active = false;
+        return this;
+    }
+
+    update() {
+        if (this.active) {
+            this.y += this.speed;
+            if (this.y > (this.displayHeight)) {
+                this.makeInactive();
+            }
+        }
+    }
+
+    makeActive() {
+        this.visible = true;
+        this.active = true;
+    }
+
+    makeInactive() {
+        this.visible = false;
+        this.active = false;
+    }
+
+}
